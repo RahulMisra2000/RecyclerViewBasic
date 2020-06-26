7c19820be787101e09e26f13721321c90b4882d3
rahul <rahulmisra2000@gmail.com>
2020-06-26 17:39:29 -0400

    Added a comment to show the location of the github repo

--- app/src/main/java/com/rahulmisra/firebaseexample/MainActivity2.java
+++ app/src/main/java/com/rahulmisra/firebaseexample/MainActivity2.java
@@ -21,12 +21,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MainActivity2 extends AppCompatActivity implements ExampleRecyclerViewAdapter.I1{
-                                                                        // MEMBER FIELDS   m
+public class MainActivity2 extends AppCompatActivity implements ExampleRecyclerViewAdapter.I1 {
+    // MEMBER FIELDS   m
     RecyclerView mRecyclerView;                                         // RecyclerView
     RecyclerView.LayoutManager lManager;                                // LayoutManager
     ExampleRecyclerViewAdapter mRecyclerViewAdapter;                    // Adapter
     ArrayList<Customer> mData = new ArrayList<>();                      // Data
+
+    FloatingActionButton mFab;
 
     // This code's Github is here: https://github.com/RahulMisra2000/RecyclerViewBasic.git
     @Override
@@ -35,34 +37,35 @@ public class MainActivity2 extends AppCompatActivity implements ExampleRecyclerV
         setContentView(R.layout.activity_main2);
 
         buildRecyclerView();
+        buildFabListener();                                                                /* C of CRUD        - Clicking the FAB button does C of CRUD*/
+    }
 
-        /* C of CRUD        - Clicking the FAB button does C of CRUD*/
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void buildFabListener() {
+        mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*Snackbar.make(v, "RRReplace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 */
-                mData.add(new Customer(R.drawable.ic_2, ((Integer) (new Random().nextInt(100))).toString(),"New"));
-                mRecyclerViewAdapter.notifyItemInserted(mData.size()-1);            // Insert the data into the last position
+                mData.add(new Customer(R.drawable.ic_2, ((Integer) (new Random().nextInt(100))).toString(), "New"));
+                mRecyclerViewAdapter.notifyItemInserted(mData.size() - 1);          // Insert the data into the last position
                 mRecyclerView.scrollToPosition(mRecyclerViewAdapter.getItemCount() - 1);    // And then scroll to it
             }
         });
-        /* C of CRUD */
     }
 
-    private void buildRecyclerView(){
+    private void buildRecyclerView() {
         mRecyclerView = findViewById(R.id.recyclerView);
 
-        getCustomers(mData);
+        getCustomers(mData);                                                                // Fill up data
 
         mRecyclerViewAdapter = new ExampleRecyclerViewAdapter(mData);
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         /*
-        There are 2 ways of sending the Activity to the Adapter :
+        There are 2 ways of sending reference to this Activity to the Adapter :
             1) Have the activity implement the interface and then send the activity (this) to the Adapter so it can call the methods
             2) Comment the above and then uncomment marker 2
          */
@@ -126,7 +129,7 @@ public class MainActivity2 extends AppCompatActivity implements ExampleRecyclerV
 
     @Override
     public void btnUpdateClicked(int pos) {
-        mData.set(pos, new Customer(R.drawable.ic_2,((Integer) (new Random().nextInt(100))).toString(), "updated"));
+        mData.set(pos, new Customer(R.drawable.ic_2, ((Integer) (new Random().nextInt(100))).toString(), "updated"));
         mRecyclerViewAdapter.notifyItemChanged(pos);
     }
 
@@ -139,8 +142,8 @@ public class MainActivity2 extends AppCompatActivity implements ExampleRecyclerV
 
     // Fill up data
     private void getCustomers(ArrayList<Customer> ac) {
-        ac.add(new Customer(R.drawable.ic_1,((Integer) (new Random().nextInt(100))).toString(),"M"));
-        ac.add(new Customer(R.drawable.ic_2,((Integer) (new Random().nextInt(100))).toString(),"M2"));
-        ac.add(new Customer(R.drawable.ic_3,((Integer) (new Random().nextInt(100))).toString(),"M3"));
+        ac.add(new Customer(R.drawable.ic_1, ((Integer) (new Random().nextInt(100))).toString(), "M"));
+        ac.add(new Customer(R.drawable.ic_2, ((Integer) (new Random().nextInt(100))).toString(), "M2"));
+        ac.add(new Customer(R.drawable.ic_3, ((Integer) (new Random().nextInt(100))).toString(), "M3"));
     }
 }
